0e01d8341c55c10a5ededf38f16287649f13ca88
Marie Sarbiewski <sarbi@kth.se>
2020-05-24 15:37:13 +0200

    Beginning the preprocessing of the textGrid

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
-import tensorflow_datasets as tfds
-from tensorflow_datasets.core import download
 import numpy as np
+import tqdm
+import os
+import re
 
-librispeech = tfds.load("librispeech", download_and_prepare_kwargs={"force_download": True} )
+TRAIN_DATA = "data/train-others-500"
 
-download_config = tfds.download.DownloadConfig(download_mode=download.GenerateMode.FORCE_REDOWNLOAD)
+
+def load_from_path(path):
+    data = []
+    for root, dirs, files in os.walk(TRAIN_DATA):
+        for file in tqdm(files):
+            if file.endswith('.wav'):
+                pass
+    return np.array(data)
+
+def read_a_line(line):
+    id, words, times = re.sub('["]','', line[:-3] ).split(' ')
+    commas_index = np.where(np.array(list(words)) == ',')[0]
+    times = times.split(',')
+
+
+    word_dict = {}
+    for i in range(len(commas_index) - 1):
+        word = words[commas_index[i]+1: commas_index[i+1]]
+        word_dict[word] = (times[i], times[i + 1])
+
+    try:
+        del word_dict['']
+    except KeyError:
+        pass
+    to_return = {}
+    to_return["id"] = id
+    to_return['times'] = word_dict
+    return id, to_return
+
+models = {}
+with open("test_seg.txt", 'r') as f:
+    for line in f.readlines():
+
+        id_temp, dict_temp = read_a_line(line)
+        models[id_temp] = dict_temp
+
