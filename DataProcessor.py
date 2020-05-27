dae32522e47cc253463d13d74180bc1e8d4c7388
Florent MONIN <florent.monin@telecom-paristech.fr>
2020-05-27 19:15:37 +0200

    Add Dataset class

--- DataProcessor.py
+++ DataProcessor.py
@@ -46,6 +46,7 @@ def log(message, file="log.log", reinit=False):
             f.write(f"{time.asctime()} : {message} \n")
 
 
+# noinspection SpellCheckingInspection
 class DataProcessor:
 
     def __init__(self, text_path, audio_path, preprocessed_data):
@@ -53,14 +54,10 @@ class DataProcessor:
         # The file containing all the forced alignment TextGrids
         self.audio_path = audio_path
         # The file containing all the audio samples
+        self.MEMORY_FEATURES = "memory/features/"
+        # The paths where the data is saved
         self.models = {}
         # A dictionary that maps id to the forced alignment for corresponding file
-        self.audio = []
-        # A 3D numpy arrays, where each 2D array corresponds to the MSpec of a word
-        # Each element of self.audio is a list of words corresponding to one file (.flac)
-        self.targets = []
-        # Contains a list of context matrices
-        # where a context matrix is the concatenation of context words feature matrices
         self.AUDIO_MAX_SIZE = 0
         # The max size of a word (for padding purposes)
         self.a2w = {}
@@ -72,28 +69,29 @@ class DataProcessor:
         self.context_size = 3
         # The right and left context size (So the total window length is 2*self.context_size)
         self.number_processed_word = 0
+        self.ids = []
         # The number of processed word we are in
+        self.mean = np.zeros(self.feature_dim)
+        self.std = np.zeros(self.feature_dim)
 
         log("Loading the alignment...", reinit=True)
         self.load_alignment()
         log("Alignment loaded")
-        if preprocessed_data + ".npz" in os.listdir():
+        if preprocessed_data + ".npz" in os.listdir("."):
             log("Loading preprocessed data from file...")
             self.load_from_file(preprocessed_data)
             log("Preprocessed data loaded from file")
         else:
             log("Preprocessing data: loading audio...")
+            os.mkdir(self.MEMORY_FEATURES)
             self.load_audio()
             log("Preprocessing data: audio loaded")
             log("Preprocessing data: padding audio...")
             self.pad()
             log("Preprocessing data: audio padded")
-            log("Preprocessing data: computing targets...")
-            self.compute_targets()
-            log("Preprocessing data: targets computed")
-            log("Preprocessing data: flattening the data...")
-            self.flatten()
-            log("Preprocessing data: data flattened")
+            log("Preprocessing data: normalizing data...")
+            self.normalize()
+            log("Preprocessing data: data normalized")
             log("Preprocessing data: saving data to file...")
             self.save_to_file(preprocessed_data)
             log("Preprocessing data: data saved to file")
@@ -116,22 +114,18 @@ class DataProcessor:
                                 log(e)
 
     def load_audio(self):
-        """Loads the audio files in self.audio
+        """Saves the features in a file
         Transforms them in a feature matrix for each word
         Word matrices are stored by file (one file is made of a collection of audio files all read by the same speaker,
         and within the same context
         """
         for speaker in os.scandir(self.audio_path):
-            print(speaker)
             for dir in os.scandir(speaker):
-                tmp = []
                 for audio_file in os.listdir(dir):
                     if audio_file.endswith('.flac'):
                         name_file = os.path.join(dir, audio_file)
-                        id = audio_file[:-5]  # 19-198-0000
-                        tmp += self.process_audio(id, name_file)
-                self.audio.append(tmp)
-        np.savez_compressed("mspec", feature=self.audio, w2a=self.w2a, a2w=self.a2w)
+                        id = audio_file[:-5]  # 19-198-0000-1.npz
+                        self.process_audio(id, name_file)
 
     def process_audio(self, id, name_file):
         """Processes one audio file
@@ -143,79 +137,55 @@ class DataProcessor:
         If there is no alignment data for this file, returns an empty list
         """
         data, sample_rate = soundfile.read(name_file)
+        i = 0
         try:
-            tmp = []
             words, times = self.models[id]
             for index, timestamp in enumerate(times):
                 start, end = timestamp
                 word_audio = data[int(start * sample_rate):int(end * sample_rate)]
                 feature_matrix = mspec(word_audio).astype('float32')
-                tmp.append(feature_matrix)
+                self.mean += np.sum(feature_matrix, axis=0)
+                self.std += np.sum(feature_matrix ** 2, axis=0)
+                np.savez(f"{self.MEMORY_FEATURES}{id}-{i}", word=feature_matrix)
+                self.ids.append(f"{id}-{i}")
                 self.AUDIO_MAX_SIZE = max(self.AUDIO_MAX_SIZE, feature_matrix.shape[0])
                 self.a2w[self.number_processed_word] = words[index]
                 self.w2a[words[index]] += [self.number_processed_word]
                 self.number_processed_word += 1
-            return tmp
+                i += 1
         except KeyError:
-            return []
+            pass
 
     def pad(self):
         """Adds 0s at the end of the 2D matrices for each word, so that each matrix has the same shape
         """
-        for i, file in enumerate(self.audio):
-            for j, word in enumerate(file):
-                padded_audio = np.zeros((self.AUDIO_MAX_SIZE, self.feature_dim), dtype='float32')
-                padded_audio[:word.shape[0]] = word
-                self.audio[i][j] = padded_audio
-            self.audio[i] = np.array(self.audio[i])
+        for file in os.listdir(self.MEMORY_FEATURES):
+            word = np.load(f"{self.MEMORY_FEATURES}{file}")
+            padded_audio = np.zeros((self.AUDIO_MAX_SIZE, self.feature_dim), dtype='float32')
+            padded_audio[:word.shape[0]] = word
+            np.savez(f"{self.MEMORY_FEATURES}{file}", word=padded_audio)
 
     def load_from_file(self, preprocessed_data):
-        """Loads the features and the targets in self.audio and self.targets respectively
+        """Loads the information about the features.
         :param preprocessed_data: The name of the .npz file where the data is stored
         """
         tmp = np.load(preprocessed_data + ".npz", allow_pickle=True)
-        self.audio = tmp['audio_features']
-        self.targets = tmp['audio_targets']
         self.w2a = tmp['w2a'].item()
         self.a2w = tmp['a2w'].item()
+        self.ids = tmp['ids']
 
     def save_to_file(self, preprocessed_data):
-        """Saves to the disk the features and the targets
-        :param preprocessed_data: The name of thr .npz where the data will be stored
+        """Saves to the disk the the information about the preprocessed data
+        :param preprocessed_data: The name of the .npz where the data will be stored
         """
-        np.savez_compressed(preprocessed_data, audio_features=self.audio, audio_targets=self.targets,
-                            a2w=self.a2w, w2a=self.w2a)
+        np.savez_compressed(preprocessed_data, a2w=self.a2w, w2a=self.w2a, ids=self.ids)
 
-    def compute_targets(self):
-        """Computes the target contexts for each word in self.audio
-        Populates self.targets with a list of 2D arrays of shape
-            (self.context_size*2*self.AUDIO_MAX_SIZE, self.feature_dim)
-        The context is constituted of the surrounding words (self.context_size words to the left, the same to the right)
-        When at the boundary, fill with zeros
+    def normalize(self):
+        """Normalizes the mspec data
         """
-        for file in self.audio:
-            n = len(file)
-            for i in range(n):
-                context = []
-                # Left context
-                while len(context) + i < self.context_size:
-                    context.append(np.zeros((self.AUDIO_MAX_SIZE, self.feature_dim)))
-                for t in range(self.context_size - len(context), 0, -1):
-                    context.append(file[i - t])
-                # Right context
-                for t in range(1, min(self.context_size, n - i)):
-                    context.append(file[i + t])
-                while len(context) < 2 * self.context_size:
-                    context.append(np.zeros((self.AUDIO_MAX_SIZE, self.feature_dim)))
-                self.targets.append(np.concatenate(context))
-
-    def flatten(self):
-        """Flattens self.audio and self.targets to 3D numpy arrays
-        """
-        self.targets = np.array(self.targets).astype('float32')
-        self.audio = np.concatenate(self.audio).astype('float32')
-
-        mean = self.audio.mean(axis=(0, 1))
-        std = self.audio.std(axis=(0, 1))
-        self.audio = (self.audio - mean)/std
-        self.targets = (self.targets - mean)/std
+        self.mean /= self.number_processed_word
+        self.std = np.sqrt(abs(self.std - self.mean ** 2))
+        for file in os.listdir(self.MEMORY_FEATURES):
+            tmp = np.load(f"{self.MEMORY_FEATURES}{file}" + ".npz", allow_pickle=True)['word']
+            tmp = (tmp - self.mean) / self.std
+            np.savez(f"{self.MEMORY_FEATURES}{file}", word=tmp)
