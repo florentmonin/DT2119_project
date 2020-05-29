4afb75ca73fbd770da66a6180e46b057572ba22c
Marie Sarbiewski <sarbi@kth.se>
2020-05-29 10:28:20 +0200

    Adding the ability to generate the embeddings and save them in a file

--- Model.py
+++ Model.py
@@ -5,18 +5,16 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, random_split
 from DataProcessor import log
+import numpy as np
 
 
 class Speech2Vec(nn.Module):
     def __init__(self, nb_frame, nb_features, embedding_size):
-        """
-        Constructs a new instance of the Speech2Vec model
+        """ Constructs a new instance of the Speech2Vec model
         :param nb_frame:        corresponds to AUDIO_MAX_SIZE in DataProcessor: max size of the padded audios
         :type nb_frame:         int
         :param embedding_size:  Size of the embedding
         :type embedding_size:   int
-        :param context_size:    Size of the context
-        :type context_size:     int
         """
 
         super(Speech2Vec, self).__init__()
@@ -49,8 +47,7 @@ class Speech2Vec(nn.Module):
         self.decoders = [self.decoder1, self.decoder2, self.decoder3, self.decoder4, self.decoder5, self.decoder6]
 
     def forward(self, X):
-        """
-        Performs a forward pass inside the Speech2Vec
+        """ Performs a forward pass inside the Speech2Vec
         :param X:   2D list of dimensionality (Batch_Size, self.INPUT_SIZE, N_features), already on CUDA
         :return     The current prediction of the model
         """
@@ -69,20 +66,43 @@ class Speech2Vec(nn.Module):
         return torch.cat(all_outputs, dim=1)
 
     def save(self, SAVE_PATH):
+        """
+        Saves the model
+        :param SAVE_PATH:   model saving path
+        """
         torch.save(self.state_dict(), SAVE_PATH + '.pyt')
         print(f"Model saved to {SAVE_PATH}.pyt.")
 
     def load(self, SAVE_PATH):
+        """
+        Loads the model
+        :param SAVE_PATH:   model saving path
+        """
         self.load_state_dict(torch.load(SAVE_PATH + '.pyt'))
+
+    def generate(self, word):
+        """ Generates the embedding of a word
+        :param word:        Word to embed, 2D np.array of shape (self.SEQ_LENGTH, self.NB_FEATURES)
+        :return:            Embedding of the word of the shape(self.EMBEDDING_SIZE,)
+        """
+        # noinspection PyArgumentList
+        tens_word = torch.Tensor(word.reshape((1, self.SEQ_LENGTH, self.NB_FEATURES)))
+        _, h_n_c_n = self.encoder(tens_word)
+        enc_last_state, _ = h_n_c_n
+        enc_last_state = enc_last_state.reshape(-1, 2 * int(self.EMBEDDING_SIZE / 2))
+        return enc_last_state.detach().numpy()[0]
 
 
 def stream(string, variables):
+    """ Streams string to std.out
+    :param string:          String to stream
+    :param variables:       Variables to format in the string above
+    """
     log(f'\r{string}' % variables, file="train.log")
 
 
 def train(model, dataset, batch_size, learning_rate=0.001, nb_epochs=500):
-    """
-    Trains the given model with the given dataset
+    """ Trains the given model with the given dataset
     :param learning_rate:   Learning rate
     :param model:           Torch Speech2Vec model to be trained
     :param dataset:         Dataset to provide for the training
@@ -141,6 +161,29 @@ def train(model, dataset, batch_size, learning_rate=0.001, nb_epochs=500):
             avg_val_loss = val_loss / (i + 1)
         val_losses.append(avg_val_loss)
         train_losses.append(avg_loss)
-        model.save('memory/models/model'+str(epoch+1))
+        model.save('memory/models/model' + str(epoch + 1))
     model.save('memory/models/model')
     return val_losses, train_losses
+
+
+def generate_embedding(model, w2a, ids, DATA_PATH, SAVE_PATH):
+    """ Generates the embedding after training the model
+    :param DATA_PATH:   Path where the features are saved
+    :param model:       Instance of Speech2Vec
+    :param w2a:         Word to list of utterances of this word mapping
+    :param ids:         Maps ids to file names
+    :param SAVE_PATH:   Path to save the embedding txt file
+    """
+    with open(SAVE_PATH, "w+") as saving_file:
+        for word in w2a.keys():
+            temp = []
+            for id in w2a[word]:
+                filename = ids[id]
+                corr_audio = np.load(DATA_PATH + filename + '.npz', allow_pickle=True)
+                corr_emb = model.generate(corr_audio)
+                temp.append(corr_emb)
+            embedding_word = np.mean(temp, axis=0)
+            saving_file.write(word)
+            for emb_value in embedding_word:
+                saving_file.write(f" {emb_value}")
+            saving_file.write('\n')
